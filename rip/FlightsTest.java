e94a1478599c563a4d612869904473382ae12ea3
dlaj23 <dlaj23@outlook.com>
2022-04-10 23:49:42 -0400

    removed unused method

--- rip/FlightsTest.java
+++ rip/FlightsTest.java
@@ -9,23 +9,6 @@ class FlightsTest {
     private ArrayList<Flight> flightList = flights.getFlights();
     private Flight flight;
     
-   /* @BeforeEach
-    public void setup(){
-        flightList.clear();
-        ArrayList<String> transferAirportCodes = new ArrayList<String>();
-        transferAirportCodes.add("N/A");
-        ArrayList<String> economyOpenSeating = new ArrayList<String>();
-        economyOpenSeating.add("A1");
-        economyOpenSeating.add("A2");
-        economyOpenSeating.add("A3");
-        ArrayList<String> firstClassOpenSeating = new ArrayList<String>();
-        firstClassOpenSeating.add("S1");
-        firstClassOpenSeating.add("S2");
-        firstClassOpenSeating.add("S3");
-        flightList.add(new Flight(1, "Columbia SC, USA", "CAE", "Charlotte NC, USA", "CLT", "12:00pm", "01/01/2022", "01:00pm", "1hr", 0, transferAirportCodes, false, true, economyOpenSeating, 50.0, 100.0, firstClassOpenSeating, 75.0, 150.0, true));
-        flightList.add(new Flight(2, "Columbia SC, USA", "CAE", "New York City NY, USA", "JFK", "12:00pm", "01/01/2022", "05:00pm", "5hr", 0, transferAirportCodes, false, false, economyOpenSeating, 75.0, 150.0, firstClassOpenSeating, 150.0, 300.0, false));
-}
-*/
 
     @Test
     void testToStringReturn(){
